706b951fbfb7ac4c066f82e3523fbe9ac91dc8e1
Najim Uddin <najim7304@gmail.com>
2022-11-02 00:41:48 +0600

    added code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      REACT CSS MODULE TWO CLASSES JOIN
+      <p className={[styles.active, styles.show].join(' ') > Hello</p>
+    </div >
+  );
+}
+
+export default App;
